522438025c7d2cc43ed4c8eb5146693360ad3150
Kunle Eniola <eniolakunle10@gmail.com>
2024-10-11 19:26:04 -0400

    Exercise 2

---
+++ Exercise_2/DinosaurIllegalArgument.java
@@ -0,0 +1,38 @@
+package Exercise_2;
+
+class Dinosaur{
+    private double weight;
+    private final String name;
+
+    Dinosaur(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+    String getName(){
+        return this.name;
+    }
+    void setWeight(double weight){
+        this.weight = weight;
+    }
+
+}
+public class DinosaurIllegalArgument {
+    static void updateDinosaurWeight(double weight, Dinosaur dinosaur) throws IllegalArgumentException{
+        if (weight < 0){
+            throw new IllegalArgumentException("Dinosaur should weigh more than zero!");
+        } else {
+            dinosaur.setWeight(weight);
+            System.out.println("New weight for " + dinosaur.getName() +
+                    ": " + weight + " lbs");
+        }
+    }
+    public static void main(String[] args) {
+        Dinosaur tRex = new Dinosaur("Bitey", 20);
+        try {
+            updateDinosaurWeight(-1, tRex);
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+        updateDinosaurWeight(25, tRex);
+    }
+}
---
+++ Exercise_2/README.md
@@ -0,0 +1,9 @@
+# Exercise 2 📚
+___
+While updating dinosaur data, invalid values could sometimes be provided. Write an
+updateDinosaurWeight method that takes a weight value and a Dinosaur object. If
+the weight value is less than zero, the method should throw IllegalArgumentException.
+Use a try-catch block to handle this exception. The handling can be a simple System.out.
+println for now
+
+![LearningWithProjects.jpg](../LearningWithProjects.jpg)
